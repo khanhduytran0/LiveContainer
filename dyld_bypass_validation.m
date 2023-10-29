f2a3c18641c4eaba59d4d440e5a19e72eabd72a2
khanhduytran0 <khanhduytran0@users.noreply.github.com>
2023-10-29 18:29:52 +0700

    Fix mmap incorrectly setting prot Sadly this fixes only PojavLauncher fishhook page permission, many apps are still broken

--- dyld_bypass_validation.m
+++ dyld_bypass_validation.m
@@ -86,29 +86,15 @@ static void *getDyldBase(void) {
 }
 
 static void* hooked_mmap(void *addr, size_t len, int prot, int flags, int fd, off_t offset) {
-    char filePath[PATH_MAX];
-    bzero(filePath, PATH_MAX);
-    
-    // Check if the file is our "in-memory" file
-    if (fd && __fcntl(fd, F_GETPATH, filePath) != -1) {
-        const char *homeDir = LCHomePath();
-        if (!strncmp(filePath, homeDir, strlen(homeDir))) {
-            int newFlags = MAP_PRIVATE | MAP_ANONYMOUS;
-            if (addr != 0) {
-                newFlags |= MAP_FIXED;
-            }
-            void *alloc = __mmap(addr, len, PROT_READ | PROT_WRITE, newFlags, 0, 0);
-            
-            void *memoryLoadedFile = __mmap(NULL, len, PROT_READ, MAP_PRIVATE, fd, offset);
-            memcpy(alloc, memoryLoadedFile, len);
-            munmap(memoryLoadedFile, len);
-            mprotect(alloc, len, prot);
-            return alloc;
-        }
+    void *map = __mmap(addr, len, prot, flags, fd, offset);
+    if (map == MAP_FAILED && (prot & PROT_EXEC)) {
+        map = __mmap(addr, len, PROT_READ | PROT_WRITE, flags | MAP_PRIVATE | MAP_ANON, 0, 0);
+        void *memoryLoadedFile = __mmap(NULL, len, PROT_READ, MAP_PRIVATE, fd, offset);
+        memcpy(map, memoryLoadedFile, len);
+        munmap(memoryLoadedFile, len);
+        mprotect(map, len, prot);
     }
-    
-    // If for another file, we pass through
-    return __mmap(addr, len, prot, flags, fd, offset);
+    return map;
 }
 
 static int hooked___fcntl(int fildes, int cmd, void *param) {
