7442b72e093137df023509a363175390cfd2f7c7
khanhduytran0 <khanhduytran0@users.noreply.github.com>
2024-01-29 21:37:07 +0700

    Fix crash in release build

--- main.m
+++ main.m
@@ -10,18 +10,19 @@
 
 #include <dlfcn.h>
 #include <execinfo.h>
+#include <libgen.h>
 #include <signal.h>
+#include <spawn.h>
 #include <sys/mman.h>
 #include <stdlib.h>
 
 static int (*appMain)(int, char**);
 
-static BOOL checkJITEnabled() {
-    // check if jailbroken
-    if (access("/var/mobile", R_OK) == 0) {
-        return YES;
-    }
+static BOOL isSandboxed() {
+    return access("/var/mobile", R_OK) != 0;
+}
 
+static BOOL isJITEnabled() {
     // check csflags
     int flags;
     csops(getpid(), 0, &flags, sizeof(flags));
@@ -165,15 +166,52 @@ static void *getAppEntryPoint(void *handle, uint32_t imageIndex) {
     return (void *)header + entryoff;
 }
 
+static BOOL enableJITSpawnPtrace(char *argv[]) {
+    if (isSandboxed()) {
+        return NO;
+    }
+
+    int pid;
+    int ret = posix_spawnp(&pid, argv[0], NULL, NULL, (char *[]){argv[0], "", NULL}, environ);
+    if (ret == 0) {
+        // Cleanup child process
+        waitpid(pid, NULL, WUNTRACED);
+        ptrace(PT_DETACH, pid, NULL, 0);
+        kill(pid, SIGTERM);
+        wait(NULL);
+
+        if (isJITEnabled()) {
+            NSLog(@"JIT has been enabled");
+#if 0
+            // Drop ourselves back to sandboxed app
+            char path[PATH_MAX];
+            sprintf(path, "%s/LiveContainer_PleaseDoNotShortenTheExecutableNameBecauseItIsUsedToReserveSpaceForOverwritingThankYou", dirname(argv[0]));
+            execv(path, (char *[]){argv[0], NULL});
+#endif
+            return 1;
+        } else {
+            NSLog(@"Failed to enable JIT: unknown reason");
+            return NO;
+        }
+    } else {
+        NSLog(@"Failed to enable JIT: posix_spawn() failed errno %d", errno);
+        return NO;
+    }
+}
+
 static NSString* invokeAppMain(NSString *selectedApp, int argc, char *argv[]) {
     NSString *appError = nil;
+
+    
     // First of all, let's check if we have JIT
-    for (int i = 0; i < 10 && !checkJITEnabled(); i++) {
-        usleep(1000*100);
-    }
-    if (!checkJITEnabled()) {
-        appError = @"JIT was not enabled";
-        return appError;
+    if (!isJITEnabled() && !enableJITSpawnPtrace(argv)) {
+        for (int i = 0; i < 10 && !isJITEnabled(); i++) {
+            usleep(1000*100);
+        }
+        if (!isJITEnabled()) {
+            appError = @"JIT was not enabled";
+            return appError;
+        }
     }
 
     NSFileManager *fm = NSFileManager.defaultManager;
@@ -211,7 +249,7 @@ static NSString* invokeAppMain(NSString *selectedApp, int argc, char *argv[]) {
 
     // Preload executable to bypass RT_NOLOAD
     uint32_t appIndex = _dyld_image_count();
-    void *appHandle = dlopen(appBundle.executablePath.UTF8String, RTLD_LAZY|RTLD_LOCAL|RTLD_FIRST);
+    void *appHandle = dlopen(*path, RTLD_LAZY|RTLD_LOCAL|RTLD_FIRST);
     const char *dlerr = dlerror();
     if (!appHandle || (uint64_t)appHandle > 0xf00000000000 || dlerr) {
         if (dlerr) {
@@ -282,6 +320,13 @@ static void exceptionHandler(NSException *exception) {
 }
 
 int LiveContainerMain(int argc, char *argv[]) {
+    if (!isJITEnabled() && argc == 2) {
+        // Child process can call to PT_TRACE_ME
+        // then both parent and child processes get CS_DEBUGGED
+        int ret = ptrace(PT_TRACE_ME, 0, 0, 0);
+        return ret;
+    }
+
     NSString *selectedApp = [NSUserDefaults.standardUserDefaults stringForKey:@"selected"];
     if (selectedApp) {
         [NSUserDefaults.standardUserDefaults removeObjectForKey:@"selected"];
