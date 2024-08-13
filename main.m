7d0facc4243e3e6bf1734ebe6e3c2eb8b8fe17ed
khanhduytran0 <khanhduytran0@users.noreply.github.com>
2024-08-13 21:53:10 +0700

    Change how tmp files are stored To provide maximum compatibility, now guest app's tmp folder is just a symlink to that of LiveContainer.

--- main.m
+++ main.m
@@ -239,11 +239,16 @@ static NSString* invokeAppMain(NSString *selectedApp, int argc, char *argv[]) {
     NSProcessInfo.processInfo.processName = appBundle.infoDictionary[@"CFBundleExecutable"];
     *_CFGetProgname() = NSProcessInfo.processInfo.processName.UTF8String;
 
-    // Overwrite home path
+    // Overwrite home and tmp path
     NSString *newHomePath = [NSString stringWithFormat:@"%@/Data/Application/%@", docPath, appBundle.infoDictionary[@"LCDataUUID"]];
+    NSString *newTmpPath = [newHomePath stringByAppendingPathComponent:@"tmp"];
+    remove(newTmpPath.UTF8String);
+    symlink(getenv("TMPDIR"), newTmpPath.UTF8String);
+
     setenv("CFFIXED_USER_HOME", newHomePath.UTF8String, 1);
     setenv("HOME", newHomePath.UTF8String, 1);
-    setenv("TMPDIR", [@(getenv("TMPDIR")) stringByAppendingFormat:@"%@/tmp/", appBundle.infoDictionary[@"LCDataUUID"]].UTF8String, 1);
+    setenv("TMPDIR", newTmpPath.UTF8String, 1);
+
     // Setup directories
     NSArray *dirList = @[@"Library/Caches", @"Documents", @"SystemData"];
     for (NSString *dir in dirList) {
