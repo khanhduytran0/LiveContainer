dc02e91dd2cee71ce57382b269a7b79f4b06d934
Huge_Black <wwkq03@outlook.com>
2024-09-17 12:24:28 +0800

    fix startAccessingSecurityScopedResource return false and not able to select folder

--- TweakLoader/DocumentPicker.m
+++ TweakLoader/DocumentPicker.m
@@ -3,22 +3,58 @@
 #import "UIKitPrivate.h"
 #import "utils.h"
 
+
+BOOL fixFilePicker;
 __attribute__((constructor))
 static void NSFMGuestHooksInit() {
+    fixFilePicker = [NSBundle.mainBundle.infoDictionary[@"doSymlinkInbox"] boolValue];
+    
     swizzle(UIDocumentPickerViewController.class, @selector(initForOpeningContentTypes:asCopy:), @selector(hook_initForOpeningContentTypes:asCopy:));
     swizzle(UIDocumentBrowserViewController.class, @selector(initForOpeningContentTypes:), @selector(hook_initForOpeningContentTypes));
-}
+    if (fixFilePicker) {
+        swizzle(NSURL.class, @selector(startAccessingSecurityScopedResource), @selector(hook_startAccessingSecurityScopedResource));
+        swizzle(UIDocumentPickerViewController.class, @selector(setAllowsMultipleSelection:), @selector(hook_setAllowsMultipleSelection:));
+    }
 
+}
 
 @implementation UIDocumentPickerViewController(LiveContainerHook)
 
 - (instancetype)hook_initForOpeningContentTypes:(NSArray<UTType *> *)contentTypes asCopy:(BOOL)asCopy {
-    NSArray<UTType *> * contentTypesNew = @[UTTypeItem, UTTypeFolder];
-    if([NSBundle.mainBundle.infoDictionary[@"doSymlinkInbox"] boolValue]) {
-        return [self hook_initForOpeningContentTypes:contentTypesNew asCopy:YES];
+    
+    // if app is going to choose any unrecognized file type, then we replace it with @[UTTypeItem, UTTypeFolder];
+    NSArray<UTType *> * contentTypesNew = contentTypes;
+    for(UTType* type in contentTypes) {
+        if(!type) {
+            contentTypesNew = @[UTTypeItem, UTTypeFolder];
+            break;
+        }
+    }
+    
+    // prevent crash when selecting only folder
+    BOOL shouldMultiselect = NO;
+    
+    if (fixFilePicker && [contentTypes count] == 1 && contentTypes[0] == UTTypeFolder) {
+        contentTypesNew = @[UTTypeItem, UTTypeFolder];
+        shouldMultiselect = YES;
+    }
+    
+    if(fixFilePicker) {
+        UIDocumentPickerViewController* ans = [self hook_initForOpeningContentTypes:contentTypesNew asCopy:YES];
+        if(shouldMultiselect) {
+            [ans hook_setAllowsMultipleSelection:YES];
+        }
+        return ans;
     } else {
         return [self hook_initForOpeningContentTypes:contentTypesNew asCopy:asCopy];
     }
+}
+
+- (void)hook_setAllowsMultipleSelection:(BOOL)allowsMultipleSelection {
+    if([self allowsMultipleSelection]) {
+        return;
+    }
+    [self hook_setAllowsMultipleSelection:YES];
 }
 
 @end
@@ -29,6 +65,16 @@ static void NSFMGuestHooksInit() {
 - (instancetype)hook_initForOpeningContentTypes:(NSArray<UTType *> *)contentTypes {
     NSArray<UTType *> * contentTypesNew = @[UTTypeItem, UTTypeFolder];
     return [self hook_initForOpeningContentTypes:contentTypesNew];
+}
+
+@end
+
+
+@implementation NSURL(LiveContainerHook)
+
+- (BOOL)hook_startAccessingSecurityScopedResource {
+    [self hook_startAccessingSecurityScopedResource];
+    return YES;
 }
 
 @end
